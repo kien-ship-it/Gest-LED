759750332d4029f713c798829ab7cde65d5ede55
dancejjjjj <daoducvinh9699@gmail.com>
2025-07-20 10:49:38 +0700

    Firmware Update
    Added: "update_leds" function

--- src/embedded_system/ESP_Firmware/ESP_Firmware.ino
+++ src/embedded_system/ESP_Firmware/ESP_Firmware.ino
@@ -1,5 +1,7 @@
 #include "config.h"
 
+/// Tested on ESP32-S3-USB-OTG
+
 void setup() {
   Serial.begin(BAUD_RATE);
 
@@ -12,7 +14,6 @@ void setup() {
 
 
 }
-
 
 String serialInput(){
   String MyString;
@@ -30,36 +31,45 @@ String serialInput(){
 }
 
 
+void update_leds(int count){
+  for(int i = count; i < LED_COUNT; i++){
+    digitalWrite(LED_PINS[i], 0);
+  }
+  for(int i = 0; i < count; i++){
+    digitalWrite(LED_PINS[i], 1);
+  }
+}
+
 void loop() {
   String A = serialInput();
 
 
   if(A == "HELLO") { /// HELLO
-    
+
     Serial.print("READY\n");
   }//////////////////////
   else if (A == "C0") {
-
+    update_leds(0);
     Serial.print("OK\n");
   }//////////////////////
   else if (A == "C1") { /// LED 1
-    
+    update_leds(1);
     Serial.print("OK\n");
   }//////////////////////
   else if (A == "C2") { /// LED 2
-
+    update_leds(2);
     Serial.print("OK\n");
   }//////////////////////
   else if (A == "C3") { /// LED 3
-    
+    update_leds(3);
     Serial.print("OK\n"); 
   }//////////////////////
   else if (A == "C4") { /// LED 4
-    
+    update_leds(4);
     Serial.print("OK\n");
   }//////////////////////
   else if (A == "C5") { /// LED 5
-    
+    update_leds(5);
     Serial.print("OK\n");
   }//////////////////////
   else if (A == ""){    /// NO DATA
--- src/embedded_system/ESP_Firmware/config.h
+++ src/embedded_system/ESP_Firmware/config.h
@@ -1,4 +1,4 @@
 // Example content of config.h
 #define BAUD_RATE 115200
 #define LED_COUNT 5
-const int LED_PINS[LED_COUNT] = {5, 4, 14, 12, 13}; // D1, D2, D5, D6, D7
+const int LED_PINS[LED_COUNT] = {42, 2, 4, 5, 6}; // D1, D2, D4, D5, D6 
